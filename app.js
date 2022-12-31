ec0341772c12aeff6f0c2af925ef3fc6c869e4a7
lingacode <lingarajpatra480@gmail.com>
2022-12-31 17:46:41 +0000

    hsbfjbh

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".products-center");
+const productsDOM = document.querySelector
+    (".products-center");
+
 // cart
 let cart = [];
+// buttons
+let buttonsDOM = [];
+
 
 // getting the products
 class Products {
@@ -54,19 +59,71 @@ class UI {
         });
         productsDOM.innerHTML = result;
     }
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll(".bag-btn")];
+        buttonsDOM = buttons;
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            let inCart = cart.find(item => item.id === id);
+            if (inCart) {
+                button.innerText = "In Cart";
+                button.disabled = true;
+            }
+            button.addEventListener('click', (event) => {
+                event.target.innerText = "In Cart";
+                event.target.disabled = true;
+                // get products from product
+                let cartItem = {...Storage.getProduct(id),amount: 1 };
+                console.log(cartItems);
+
+                // add product to the cart
+                cart = [...cart,cartItem];
+                // save cart in local storage
+                Storage.saveCart(cart);
+                // set cart values
+                this.setCartValues(cart);
+                // display cart item
+                // show the cart
+            });
+
+        });
+    }
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        });
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+        console.log(cartTotal,cartItems);
+    }
 }
 // local storage
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
     }
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+    static saveCart(cart) {
+        localStorage.setItem('cart',JSON.stringify(cart));
+    }
 }
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
     // get all products
-    products.getProducts().then(products => {
-        ui.displayProducts(products)
-        Storage.saveProducts(products);
-    });
+    products
+        .getProducts()
+        .then(products => {
+            ui.displayProducts(products)
+            Storage.saveProducts(products);
+        })
+        .then(() => {
+            ui.getBagButtons();
+        });
 });
